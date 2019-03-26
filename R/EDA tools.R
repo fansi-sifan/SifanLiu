5f4f0587a44e0dd24aaf507aa573f1b81168f053
fansi-sifan <sifan1121@gmail.com>
2019-03-26 09:30:11 -0400

    add "read_all" function

--- R/EDA tools.R
+++ R/EDA tools.R
@@ -6,11 +6,12 @@ padz <- function(x, n=max(nchar(x)))gsub(" ", "0", formatC(x, width=n))
 # Share of NA values
 na.share <- function(x)(sum(is.na(x))/length(x))
 
-# check distribution
-range <- function(x)(summary(as.factor(x)))
-
 # quickly summarize factor levels
 sfactor <- function(x)summary(as.factor(as.character(x)))
 
+# read all csv files in a folder and bind_rows
+read_all <- function(file.location){
+  map_df(list.files(file.location, full.names = T), read.csv)
+}
 
 
