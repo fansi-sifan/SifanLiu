276511e97e0f919284092d5aad3bd9050c018069
fansi-sifan <sifan1121@gmail.com>
2019-05-16 15:41:34 -0400

    further clean the place2county names

---
+++ data-raw/pl2co.R
@@ -0,0 +1,14 @@
+# XWALKS: place2county ==========================
+
+# further clean the place names
+library(dplyr)
+place2county <- metro.data::place2county
+
+pl2co <- place2county  %>%
+  mutate(pl_label = gsub(" city| town| CDP| village| municipality| borough", "", pl_name),
+         pl_label = tolower(gsub("\\,.+", "", pl_label)),
+         pl_label = ifelse(grepl("(balance)",pl_label),
+                           gsub(" \\(balance\\)","",gsub(" \\w.+|\\-\\w.+","",pl_label)),
+                           pl_label),
+         st_name = state.name[match(st_ab,state.abb)])
+
